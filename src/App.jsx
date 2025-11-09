d7103cd1e95b716b82a9e23e72c767070bc5a490
Its-Delimas <spencerdelimas@gmail.com>
2025-11-09 13:17:55 +0300

    array.map() function and destructuring fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const movies=[
+{title:"Superman",year:2025,id:1,isGood:true},
+{title:"Ne Zha",year:2019,id:2,isGood:false},
+{title:"Batman",year:2020,id:3,isGood:true}
+]
 
+export default function App(){
+ const movieList= movies.map(({title,year,id,isGood})=>
+
+    <li key={id} style={{color:isGood?'magenta':'orangered'}}>
+       {id} {title},{year}
+    </li>
+
+ )
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ol>
+      {movieList}
+    </ol>
+  );
 }
-
-export default App
